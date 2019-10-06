25459138d34f36bd9e9f4acf3ddabc24783c37bf
root <root@master.test-openshift.com>
2019-10-06 11:37:19 -0400

    change

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello PHP 7.0 OpenShift Container Platform!!!Hi StarHub Openshift Test 2019</p>'; ?> 
+ <?php echo '<p>Hello PHP 7.0 OpenShift Container Platform!!!Need tom complete Openshift Exam By 2019 October</p>'; ?> 
  </body>
 </html>
